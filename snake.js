58b110f5865850318d4b723eaea3e7648ff23422
Ellie575 <elliekubo@hotmail.com>
2022-08-30 15:02:51 +1000

    Extended body and tweaked movement

--- snake.js
+++ snake.js
@@ -3,6 +3,8 @@ const snakeBody = [
   { x: 10, y: 11 },
   { x: 11, y: 11 },
   { x: 12, y: 11 },
+  { x: 13, y: 11 },
+  { x: 14, y: 11 },
 ];
 
 export function update() {
@@ -10,8 +12,8 @@ export function update() {
     snakeBody[i + 1] = { ...snakeBody[i] };
   }
 
-  snakeBody[0].x += 1;
-  snakeBody[0].y += 0;
+  snakeBody[0].x += 0;
+  snakeBody[0].y += 1;
 }
 
 export function draw(gameBoard) {
