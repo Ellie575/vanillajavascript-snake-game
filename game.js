e0e612d6b02babc0adbc0977f5ad16668e9a120d
Ellie575 <elliekubo@hotmail.com>
2022-08-30 13:26:50 +1000

    Added snake.js and update and draw functions

--- game.js
+++ game.js
@@ -1,13 +1,28 @@
+import {
+  update as updateSnake,
+  draw as drawSnake,
+  SNAKE_SPEED,
+} from "./snake.js";
+
 let lastRenderTime = 0;
-const SNAKE_SPEED = 2;
 
 function main(currentTime) {
   window.requestAnimationFrame(main);
   const secondsSinceLastRendered = (currentTime - lastRenderTime) / 1000;
   if (secondsSinceLastRendered < 1 / SNAKE_SPEED) return;
 
-  console.log("Render");
   lastRenderTime = currentTime;
+
+  update();
+  draw();
+
+  function update() {
+    updateSnake();
+  }
+}
+
+function draw() {
+  drawSnake();
 }
 
 window.requestAnimationFrame(main);
